6749a77951423f485e84a1f08677f021b68c72f0
armandoltx <armando@armandoltx.com>
2017-10-13 19:54:53 +1100

    Armandoltx initial commit

---
+++ lib/armandoltx_view_tool.rb
@@ -0,0 +1,6 @@
+require "armandoltx_view_tool/version"
+require "armandoltx_view_tool/renderer"
+
+module ArmandoltxViewTool
+  # Your code goes here...
+end
---
+++ lib/armandoltx_view_tool/version.rb
@@ -0,0 +1,3 @@
+module ArmandoltxViewTool
+  VERSION = "0.1.0"
+end
